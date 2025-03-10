5efe8b6652be867a09f2292a466bece2347a16db
Anton <garynets.anton@gmail.com>
2025-03-10 12:29:00 +0200

    Added model for reviews

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text_review = models.TextField()
+    date = models.DateTimeField(auto_now=True)
+    rating = models.SmallIntegerField()
+    movie_id = models.PositiveIntegerField()
