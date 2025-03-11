76c5ca0bdbb5fb6af729653e114e978940ba61b2
Anton <garynets.anton@gmail.com>
2025-03-11 21:46:16 +0200

    Added __str__() method for Review class

--- movie/models.py
+++ movie/models.py
@@ -7,3 +7,6 @@ class Review(models.Model):
     date = models.DateTimeField(auto_now=True)
     rating = models.SmallIntegerField()
     movie_id = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"user: {self.user}, review: {self.text_review}, date: {self.date}, rating: {self.rating}, movie id: {self.movie_id}"
