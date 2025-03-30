01504cdcfc60568410d8424166ad6f2986324a72
Anton <garynets.anton@gmail.com>
2025-03-30 15:27:40 +0300

    Added model for user profile images

--- movie/models.py
+++ movie/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django_resized import ResizedImageField
 
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -10,3 +11,11 @@ class Review(models.Model):
 
     def __str__(self):
         return f"user: {self.user}, review: {self.text_review}, date: {self.date}, rating: {self.rating}, movie id: {self.movie_id}"
+
+
+class ProfileUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = ResizedImageField(size=[1280, 1280], crop=['middle', 'center'], blank=True, default="profile_images/default_profile_image.png", upload_to="profile_images/")
+
+    def __str__(self):
+        return f"{self.profile_image}"
